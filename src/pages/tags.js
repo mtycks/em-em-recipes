29faf8e362b2948a74e4b5c46cfeccdbed1e57f1
Matthew-Mesa <matthewmesa@gmail.com>
2020-03-01 13:31:16 -0800

    Added Tags page

---
+++ src/pages/tags.js
@@ -0,0 +1,84 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { Row, Col } from 'reactstrap'
+import RecipeCard from '../components/recipe-card'
+import { slugify } from '../utils/utilityFunctions'
+
+class TagsPage extends React.Component {
+  render() {
+    const { data } = this.props
+    const siteTitle = data.site.siteMetadata.title
+    const posts = data.allMarkdownRemark.edges
+    const tags = data.tags.group
+
+    return (
+      <Layout location={this.props.location} title={siteTitle}>
+        <SEO title="View all Em Em Recipes" />
+
+        <div className="container">
+
+          <Row>
+
+          {tags.map(({ tag, totalCount }) => {
+              return (
+                <Col key={tag.tag} sm="4">
+                  <div className="tag-holder">
+                    <Link to={`/tags/${slugify(tag)}`} className="portfolio-item tag-item p-3">
+                      {tag} <small className="d-none d-sm-inline-block">{totalCount} recipe{totalCount > 1 ? `s` : ``}</small>
+                    </Link>
+                  </div>
+                </Col>
+              )
+            })}
+
+          </Row>
+
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export default TagsPage
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+            prep
+            cook
+            thumbnail{
+              childImageSharp{
+                fluid(maxWidth:500, quality:80){
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    tags: allMarkdownRemark(limit: 2000){
+      group(field: frontmatter___tags) {
+        tag: fieldValue
+        totalCount
+      }
+    }
+  }
+`
