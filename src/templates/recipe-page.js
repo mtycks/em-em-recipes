c0571ef1ad749a44f5d4dc57d88ee7d8ee766076
David Anderson <david@alpinegizmo.com>
2020-12-24 19:44:16 +0100

    reverse directions for recipe nav, so they match front page

--- src/templates/recipe-page.js
+++ src/templates/recipe-page.js
@@ -124,16 +124,16 @@ class RecipePageTemplate extends React.Component {
                 <nav>
                   <ul className="recipe-footer-nav list-inline">
                     <li className="list-inline-item">
-                      {previous && (
-                        <Link to={previous.fields.slug} rel="prev">
-                          ← {previous.frontmatter.title}
+                      {next && (
+                        <Link to={next.fields.slug} rel="next">
+                          ← {next.frontmatter.title}
                         </Link>
                       )}
                     </li>
                     <li className="list-inline-item">
-                      {next && (
-                        <Link to={next.fields.slug} rel="next">
-                          {next.frontmatter.title} →
+                      {previous && (
+                        <Link to={previous.fields.slug} rel="prev">
+                          {previous.frontmatter.title} →
                         </Link>
                       )}
                     </li>
